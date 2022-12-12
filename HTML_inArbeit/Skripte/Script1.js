75c85e2110b8430b8aa22ad9047b44fb92618b3c
djey8 <janniskluess@yahoo.de>
2022-12-12 18:35:34 +0100

    design + clean up

--- HTML_inArbeit/Skripte/Script1.js
+++ HTML_inArbeit/Skripte/Script1.js
@@ -1,4 +1,12 @@
-for (let i = 0; i < 1; i++) {      
-    let z = i + 1 
-    document.write("Hallo Kurt! Versuch: "+z);
-}
+var colors = ["Azure", "Blue", "Aquamarine", "DarkBlue", "LightBlue", "DarkSeaGreen","CornflowerBlue",
+                "CadetBlue","DarkSlateBlue","DodgerBlue","LightSeaGreen","MidnightBlue","Navy"];
+            var i = 1;
+    
+window.setInterval(function(){
+    document.body.style.backgroundColor = colors[i];
+    i++;
+    if (i === colors.length){
+        i=0;
+    }
+            
+}, 18000);
